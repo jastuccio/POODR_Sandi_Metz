e0dc30b0c035c8830b8877e8794c706299dc8ac0
Joe <joe@astucc.io>
2014-12-24 13:18:17 -0500

    moved wheel to its own class

--- metz_bicyle_calc.rb
+++ metz_bicyle_calc.rb
@@ -1,11 +1,9 @@
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
-  
-  def initialize (chainring, cog, rim, tire)
+  attr_reader :chainring, :cog, :wheel
+  def initialize(chainring, cog, wheel=nil)
     @chainring = chainring
     @cog       = cog
-    @rim       = rim
-    @tire      = tire
+    @wheel     = wheel
   end
 
   def ratio
@@ -13,11 +11,32 @@ class Gear
   end
 
   def gear_inches
-      # tire goes around rim twice for diameter
-    ratio * ( rim + (tire * 2))
+    ratio * wheel.diameter
   end
-  
 end
 
-puts Gear.new(52, 11, 26, 1.5).gear_inches
-puts Gear.new(52, 11, 24, 1.25).gear_inches
+class Wheel
+  attr_reader :rim, :tire
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
+
+@wheel = Wheel.new(26, 1.5)
+puts @wheel.circumference
+# -> 91.106186954104
+
+puts Gear.new(52, 11, @wheel).gear_inches
+# -> 137.090909090909
+
+puts Gear.new(52, 11).ratio
+# -> 4.72727272727273
